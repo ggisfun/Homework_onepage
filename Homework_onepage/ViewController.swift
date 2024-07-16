47d746a5cdae70b1195609dce2b0a4acab936f31
Adam <bgisgone@gmail.com>
2024-07-16 19:20:34 +0800

    Initial Commit

---
+++ Homework_onepage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_onepage
+//
+//  Created by Adam Chen on 2024/7/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
